0a3ea443fdd0b06af80bfdd7ffd036f51ab9f852
tj-bu-706-21 <tj-bu-706-21@DESKTOP-5AQKN5H>
2025-03-31 17:27:47 +0900

    Q11

---
+++ Mission_SYS/src/Q11/Account.java
@@ -0,0 +1,103 @@
+package Q11;
+
+import java.util.ArrayList;
+
+public class Account {
+
+	private String accountNumber;
+	private String name;
+	private int balance;
+	private int passWord;
+	
+	
+	
+	public Account() {
+		this("계좌없음", "이름없음", 0, 0);
+	}
+	public Account(String accountNumber, String name, int balance, int passWord) {
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.balance = balance;
+		this.passWord = passWord;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public int getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(int passWord) {
+		this.passWord = passWord;
+	}
+	@Override
+	public String toString() {
+		return "" + accountNumber + " \t\s " + name + " \t\t " + balance + " ";
+	}
+
+
+}
+
+interface AccountInterface {
+	int BALANCE_MAX = 1000000000;
+	int MAX_DEPOSIT_AMOUNT = 1000000;
+	
+	Account create(Account account);
+	
+	ArrayList<Account> list();
+	
+	Account read(String accountNumber);
+	
+}
+
+class AccountAccess implements AccountInterface {
+	
+	int count = 0;
+	ArrayList<Account> accountList = new ArrayList<>();
+	
+	public AccountAccess() {
+	}
+	
+	@Override
+	public Account create(Account account) {
+		accountList.add(account);
+		System.out.println("계좌가 개설되었습니다.");
+		return account;
+	}
+
+	@Override
+	public ArrayList<Account> list() {
+		if (accountList.isEmpty()) {
+			System.out.println("개설된 계좌가 없습니다.");
+			
+			return null;
+		}
+		System.out.println("========== 계좌 목록 ===========");
+		System.out.println("예금주 \t\s 계좌번호 \t\t 잔고");
+		return accountList;
+	}
+
+	@Override
+	public Account read(String accountNumber) {
+		for (Account account : accountList) {
+			if (accountNumber.equals(account.getAccountNumber()))
+			return account;
+		}
+		return null;
+	}
+
+}
---
+++ Mission_SYS/src/Q11/Bank.java
@@ -0,0 +1,138 @@
+package Q11;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Bank {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AccountInterface accountInterface = new AccountAccess();
+		int num;
+		
+		do {
+			System.out.println("============================");
+			System.out.println("1. 계좌 등록");
+			System.out.println("2. 입금");
+			System.out.println("3. 출금");
+			System.out.println("4. 계좌 조회");
+			System.out.println("5. 계좌 목록");
+			System.out.println("6. 종료");
+			System.out.println("============================");
+			System.out.print("입력 : ");
+			num = sc.nextInt();
+			sc.nextLine();
+			
+			if (num == 0) break;
+			
+			switch (num) {
+			case 1:
+				System.out.print("계좌번호>>");
+				String accountNumber = sc.nextLine();
+				
+				System.out.print("예금주>>");
+				String name = sc.nextLine();
+				
+				System.out.print("최초예금액>>");
+				int firstDeposit = sc.nextInt();
+				sc.nextLine();
+				
+				System.out.print("비밀번호>>");
+				int pw = sc.nextInt();
+				sc.nextLine();
+				
+				Account creatAccount = new Account(accountNumber, name, firstDeposit, pw);
+
+				break;
+			case 2:
+				System.out.print("계좌번호>>");
+				String accNumber = sc.nextLine();
+				Account accountNum = accountInterface.read(accNumber);
+				if (accountNum != null) {
+					System.out.print("입금액>>");
+				} else {
+					System.out.println("계좌가 없습니다.");
+				}
+				int money = sc.nextInt();
+				System.out.println("'" + accountNum.getName() + "'" +"님에게 입금하는게 맞으십니까?");
+				System.out.println("1. 예");
+				System.out.println("2. 아니오");
+				System.out.print("입력>>");
+				int chooseNum = sc.nextInt();
+				
+				if (chooseNum == 1) {
+					accountNum.setBalance(accountNum.getBalance() + money);
+					System.out.println("입금되었습니다.");
+					System.out.println("잔고는 " + accountNum.getBalance() + "원 입니다.");
+				} else if (chooseNum == 2){
+					System.out.println("입금이 취소되었습니다.");
+				}
+			
+				break;
+			case 3:
+				System.out.print("계좌번호>>");
+				accNumber = sc.nextLine();
+				
+				System.out.print("비밀번호>>");
+				int accPw = sc.nextInt();
+				sc.nextLine();
+				
+				Account account = accountInterface.read(accNumber);
+				boolean accChk = accNumber.equals(account.getAccountNumber());
+
+				boolean pwChk = accPw == account.getPassWord();
+				if (!accChk) {
+					System.err.println("계좌번호가 틀립니다.");
+				} else if (accChk && pwChk) {
+					System.out.print("출금액>>");
+					int withDraw = sc.nextInt();
+					sc.nextLine();
+					System.out.println("'" + account.getName() + "'" + "님의 계좌에서 " + withDraw + "원이 출금되었습니다.");
+					account.setBalance(account.getBalance()-withDraw);
+					System.out.println("잔액은 " + account.getBalance() + "원 입니다.");
+				}
+				break;	
+			case 4:
+				System.out.println("============= 계좌 조회 =============");
+				System.out.print("계좌번호>>");
+				String acNum = sc.nextLine();
+				System.out.print("비밀번호>>");
+				int acPw = sc.nextInt();
+				Account acc = accountInterface.read(acNum);
+				boolean accInquiry = acNum.equals(acc.getAccountNumber());
+				boolean accPwInquiry = acPw == acc.getPassWord();
+				if (accInquiry && accPwInquiry)
+					System.out.println("'" + acc.getName() + "'" + "님의 계좌 잔액은 " + acc.getBalance() + "원 입니다.");
+				if (!accInquiry) {
+					System.err.println("없는 계좌번호 입니다.");
+				} else if(!accPwInquiry) {
+					System.err.println("비밀번호가 틀렸습니다.");
+				}
+				break;
+			case 5:
+				System.out.print("입력>>");
+				int adminPw = sc.nextInt();
+				if (adminPw == 1234) {
+					ArrayList<Account> accountList = (ArrayList<Account>) accountInterface.list();
+					for (Account accarray : accountList) {
+						System.out.println(accarray);
+					}
+				} else {
+					System.err.println("비밀번호가 다릅니다!");
+				}
+				break;
+			default:
+				if (num == 6) {
+					System.out.println("시스템을 종료합니다.");
+				} else
+				System.out.println("1~6번 사이의 숫자를 입력해주세요.");
+				break;
+		}
+	} while (true);
+		System.out.println("종료합니다.");
+		sc.close();
+		
+	}
+	
+}
